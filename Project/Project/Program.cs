b3f6eb7ecdba7abd245fd650c61e0ed95c4ab264
DESKTOP-J1O475E\vmel2 <vmel2947@gmail.com>
2018-05-20 22:57:32 +0300

    Implemented Task using Threads

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -20,6 +20,7 @@ namespace Project
         {
             Console.WriteLine("Set scan path :");
             path = Console.ReadLine();
+            Console.WriteLine();
 
             if (!Directory.Exists(path))
             {
@@ -29,27 +30,47 @@ namespace Project
             }
             else
             {
-                CreateScanThread(path);
+                var waitHandle = new AutoResetEvent(false);
+                CreateScanThread(new ScanData(path, waitHandle));
+                waitHandle.WaitOne();
             }
         }
 
-        static void CreateScanThread(string scanPath)
+        static void CreateScanThread(ScanData scanData)
         {
-            new Thread(Scan).Start(scanPath);
+            new Thread(Scan).Start(scanData);
         }
 
-        static void Scan(object pathObj)
+        static void Scan(object scanDataObj)
         {
-            var path = (string)pathObj;
-            
+            var scanData = scanDataObj as ScanData;
+                    
             lock (locker)
             {
-                data.Add(path);
-                Console.WriteLine(path);
+                data.Add(scanData.path);
+                Console.WriteLine(scanData.path);
             }
 
-            foreach (string dirPath in Directory.GetDirectories(path))
-                CreateScanThread(dirPath);
+            foreach (string dirPath in Directory.GetDirectories(scanData.path))
+            {
+                AutoResetEvent waitHandle = new AutoResetEvent(false);
+                CreateScanThread(new ScanData(dirPath, waitHandle));
+                waitHandle.WaitOne();
+            }
+
+            scanData.waitHandle.Set();
+        }
+    }
+
+    class ScanData
+    {
+        public string path;
+        public AutoResetEvent waitHandle;
+
+        public ScanData(string path, AutoResetEvent waitHandle)
+        {
+            this.path = path;
+            this.waitHandle = waitHandle;
         }
     }
 }
